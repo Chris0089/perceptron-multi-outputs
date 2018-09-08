e49d0becfcef30056b7bfac9bbd10b6399866b29
Christian Amparo <camparo89@gmail.com>
2018-09-08 02:24:12 -0500

    Finished. It lacks of printed values, but with a debugger works nice

--- perceptron.py
+++ perceptron.py
@@ -1,8 +1,9 @@
 import random
+import csv
 
 INPUTFILE = 'input-values.txt'
 DESIREDFILE = 'desired-values.txt'
-ETA = 0.4
+ETA = 0.1
 BOTTOM_LIMIT = -1
 TOP_LIMIT = 1
 
@@ -35,7 +36,6 @@ class DataAccessObject:
                     self.desiredOutput.append([])
             for value in range(0, size):
                 self.desiredOutput[value].append(float(fields[value]))
-        x=1
 
 
 class Perceptron(DataAccessObject):
@@ -46,6 +46,8 @@ class Perceptron(DataAccessObject):
     threshold = []  # bias
     output = []
     desiredOutput = []
+    desiredColumns = None
+    desiredRows = None
     v = []
     summation = []
     iteration = 0
@@ -54,30 +56,40 @@ class Perceptron(DataAccessObject):
         self.inputData = DataAccessObject.inputData.copy()
         self.desiredOutput = DataAccessObject.desiredOutput.copy()
         self.inputQuantity = len(self.inputData)
-        self.inputSize = len(self.desiredOutput)
+        self.inputSize = len(self.inputData[0])
+        self.desiredColumns=len(self.desiredOutput)
+        self.desiredRows = len(self.desiredOutput[0])
         self.generate_random_values()
         self.main_algorithm()
 
     def generate_random_values(self):
-        for value in range(0, self.inputQuantity):
+        for column in range(0, self.desiredColumns):
             self.weightData.append([])
-        for value in range(0, self.inputQuantity):
-            for inputSize in range(0, self.inputSize):
-                self.weightData[value].append(random.uniform(BOTTOM_LIMIT, TOP_LIMIT))
-        for size in range(0, self.inputSize):
-            self.threshold.append(random.uniform(BOTTOM_LIMIT, TOP_LIMIT))
+            self.threshold.append([])
+            for value in range(0, self.inputQuantity):
+                self.weightData[column].append([])
+            for value in range(0, self.inputQuantity):
+                for inputSize in range(0, self.inputSize):
+                    self.weightData[column][value].append(random.uniform(BOTTOM_LIMIT, TOP_LIMIT))
+            for size in range(0, self.inputSize):
+                self.threshold[column].append(random.uniform(BOTTOM_LIMIT, TOP_LIMIT))
 
     def calculate_output(self):
         self.output = []
         self.summation = []
         self.v = []
-        for row in range(0, self.inputSize):
-            self.summation.append(0)
-            self.v.append(0)
-            for column in range(0,self.inputQuantity):
-                self.summation[row] += self.inputData[column][row] * self.weightData[column][row]
-            self.v[row] = self.summation[row] + self.threshold[row] #changed, summed bias
-            self.output.append(self.activation_function(self.v[row]))
+        for column in range(0, self.desiredColumns):
+            self.output.append([])
+            self.summation.append([])
+            self.v.append([])
+            for row in range(0, self.inputSize):
+                self.summation[column].append(0)
+                self.v[column].append(0)
+                for columnInput in range(0,self.inputQuantity):
+                    self.summation[column][row] += \
+                        self.inputData[columnInput][row] * self.weightData[column][columnInput][row]
+                self.v[column][row] = self.summation[column][row] + self.threshold[column][row] #changed, summed bias
+                self.output[column].append(self.activation_function(self.v[column][row]))
         self.iteration += 1
 
     def activation_function(self, value):
@@ -93,22 +105,23 @@ class Perceptron(DataAccessObject):
             return False
 
     def training(self):
-        for row in range(0,self.inputSize):
-            if self.desiredOutput[row] != self.output[row]:
-                error = self.desiredOutput[row] - self.output[row]
-                self.threshold[row] = self.threshold[row] + (ETA * error)
-                for column in range(0, self.inputQuantity):
-                    self.weightData[column][row] = self.weightData[column][row] +\
-                                                   (ETA * error * self.inputData[column][row])
+        for column in range(0, self.desiredColumns):
+            for row in range(0,self.inputSize):
+                if self.desiredOutput[column][row] != self.output[column][row]:
+                    error = self.desiredOutput[column][row] - self.output[column][row]
+                    self.threshold[column][row] = self.threshold[column][row] + (ETA * error)
+                    for columnInput in range(0, self.inputQuantity):
+                        self.weightData[column][columnInput][row] = self.weightData[column][columnInput][row] +\
+                                                       (ETA * error * self.inputData[columnInput][row])
 
     def main_algorithm(self):
         self.calculate_output()
-        self.print_data()
+        #self.print_data()
         while not self.is_the_desired_output():
             print(self.iteration)
             self.training()
             self.calculate_output()
-            self.print_data()
+            #self.print_data()
         print("Finished")
 
     def print_data(self):
@@ -120,11 +133,11 @@ class Perceptron(DataAccessObject):
                    + str(self.desiredOutput[row]) + " | "
                    + str(self.inputData[0][row]) + " | "
                    + str(self.inputData[1][row]) + " | "
-                   + str(round(self.weightData[0][row],5)) + " | "
-                   + str(round(self.weightData[1][row],5)) + " | "
-                   + str(round(self.threshold[row],5)) + " | "
+                   + str(self.weightData[0][row]) + " | "
+                   + str(self.weightData[1][row]) + " | "
+                   + str(self.threshold) + " | "
                    )
 
 dao = DataAccessObject()
-#perceptron = Perceptron()
+perceptron = Perceptron()
 
